06569ee511bf95b6b7230c251f90e6026dc087fe
Muhammad-Rafeh <m.rafeh77@gmail.com>
2021-09-04 15:24:01 +0500

    this is 3rd day commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,59 @@
 import logo from './logo.svg';
 import Home from './components/Home'
 import './App.css';
+import { motion } from 'framer-motion';
+import MyImg from './images/MyImg.jpg';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delay : 3,
+      delayChildren: 0.5
+    }
+  }
+}
+
+const name={
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delay : 3.5,
+      delayChildren: 0.5
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="nav-wrapper">
-          <h2 className="logo-h2" ><span>Full</span> Stack Developer</h2>
-          <h3 className="name"> Muhammad Rafeh</h3>
-        </div>
-        <hr className="hr" />
-        <Home />
-        
-        
-
+        <motion.div 
+           animate={{x :[-600 , -50] , y:[0 , -600] } }
+           transition={{x:{duration:2}},{y : {delay : 2}}}
+           
+        >
+          <img src={MyImg} className="img-entry"/>
+        </motion.div>
+        <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+       >
+          <div className="nav-wrapper">
+              <h2 className="logo-h2" ><span>Full</span> Stack Developer</h2>
+              <motion.h3 
+                variants={name}
+                initial="hidden"
+                animate="show"
+                
+              className="name"> Muhammad Rafeh</motion.h3>
+            </div>
+            <hr className="hr" />
+          <Home />
+        </motion.div>
       </header>
     </div>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import MyImg from '../images/MyImg.jpg';
+import WeatherApp from '../images/weatherApp.png';
+import ChatApp from '../images/chatApp.png';
+import Ecommerce from '../images/ecommerce.png';
 import './home.css';
+
+import { motion } from 'framer-motion';
 function Home() {
     return (
         <div className="home-wrapper">
             <div className="info-wrapper">
                 <div className="protfolio">
-                    Protfolio
+                    <div>Work</div>
+                    <div className="prot-items-wrapper">
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <img className="weather-app" src={WeatherApp} />
+                            <div className="github">GitHub</div>
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <img className="chat-app" src={ChatApp} />
+                            <div className="github">GitHub</div>
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <img className="ecommerce-app" src={Ecommerce} />
+                            <div className="github">GitHub</div>
+                        </motion.div>
+                    </div>
                 </div>
                 <div className="skills">
                     <div>
@@ -14,8 +42,47 @@ function Home() {
                     </div>
                    
                     <div className="skill-category-wrapper">
-                        <div>React JS</div>
-                        <div>Node JS</div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >React JS</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >Node JS</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >Mongo DB</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >Express</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >Redux</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >Javacript</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >HTML</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >CSS</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >BootStrap</motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                        >FireBase</motion.div>
+                        {/* <div>Node JS</div>
                         <div>Mongo DB</div>
                         <div>Express</div>
                         <div>Redux</div>
@@ -23,11 +90,11 @@ function Home() {
                         <div>HTML</div>
                         <div>CSS</div>
                         <div>BootStrap</div>
-                        <div>FireBase</div>
+                        <div>FireBase</div> */}
                     </div>
                 </div>
                 <div className="moto">
-                    moto
+                    "To guarantee the utmost satisfaction of my clients."
                 </div>
             </div>
             <img className="my-img" src={MyImg} alt="mt img" />
